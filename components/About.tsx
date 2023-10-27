757217f68beb20b77c315fae4783d9ab8698f62b
Mayank Bharati <75744167+MayankBharati@users.noreply.github.com>
2023-10-27 20:57:13 +0000

    changes

--- components/About.tsx
+++ components/About.tsx
@@ -13,28 +13,16 @@ const About = () => {
       <div className="flex flex-col lgl:flex-row gap-16">
         <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
           <p>
-            Hello! My name is Noor Mohammad and I enjoy creating things that
-            live on the internet. My interest in web development started back in
-            2012 when I decided to try editing custom Tumblr themes — turns out
-            hacking together a custom reblog button taught me a lot about HTML &
-            CSS!
+            Hello! My name is Mayank Bharati and I enjoy creating things that
+            live on the internet and Quantitative Finance. I got introduced to programming in my freshman year at <a href="https://www.iiita.ac.in/"><span className="text-textGreen">
+              Indian Institute of Information Technology, Allahabad
+          </span></a> where I completed my Bachelors in Electronics and Coomunications Engineering (2020-2024) with <span className="text-textGreen">8.33 CGPA</span>.
           </p>
           <p>
-            Fast-forward to today, and I had the privilege of working at an
-            advertising agency, a start-up,{" "}
-            <span className="text-textGreen">
-              a huge corporation, and a student-led design studio.
-            </span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio rem
-            accusantium fugiat veritatis, quidem repellat{" "}
-            <span className="text-textGreen">
-              ab natus possimus? Sint, accusamus!
-            </span>
+          I'm a passionate creator who loves to use my coding skills to make a positive impact. When I'm not immersed in the world of technology, I enjoy embarking on culinary adventures, tinkering with electronics, and occasionally indulging in underrated TV series.
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-            ullam eligendi dolorum nostrum possimus quos iusto magnam ut esse
-            recusandae.
+          My commitment to open source projects is unwavering, and I take pleasure in both building and contributing to these initiatives. Teaching and assisting others is a source of joy for me.
           </p>
           <p>Here are a few technologies I have been working with recently:</p>
           <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6">
@@ -48,43 +36,37 @@ const About = () => {
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
+              C++/C
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
+              Spring MVC & Maven
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
+              Kafka & Couchbase
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
+              Quantitative finance
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
+              Python
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
                 <AiFillThunderbolt />
               </span>
-              JavaScript (ES6+)
-            </li>
-            <li className="flex items-center gap-2">
-              <span className="text-textGreen">
-                <AiFillThunderbolt />
-              </span>
-              Next.js
+              Jupyter Notebook
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
@@ -103,12 +85,6 @@ const About = () => {
                 <AiFillThunderbolt />
               </span>
               TypeScript
-            </li>
-            <li className="flex items-center gap-2">
-              <span className="text-textGreen">
-                <AiFillThunderbolt />
-              </span>
-              Express.js
             </li>
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
